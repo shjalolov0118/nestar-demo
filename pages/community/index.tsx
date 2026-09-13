304999851b2a8dc30d64ef1737676197faa7b511
Shukurjon/LEO <jalolovshukurjon16@gmail.com>
2026-09-14 00:11:04 +0900

    feat: integration Header and Footer via Layouts

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,9 +1,9 @@
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { NextPage } from "next";
 import { useState } from "react";
 
-const Community = () => {
-  console.log("COMMUNITY COMPONENT - PAGES ROUTER");
+const Community: NextPage = () => {
   const [title, setTitle] = useState<string>("hello");
-
   return (
     <div>
       COMMUNITY{" "}
@@ -14,4 +14,4 @@ const Community = () => {
   );
 };
 
-export default Community;
+export default withLayoutBasic(Community);
