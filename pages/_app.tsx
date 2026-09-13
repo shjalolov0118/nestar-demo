703f6b5305d990f59c81bcf1ce0408a9b3238259
Shukurjon/LEO <jalolovshukurjon16@gmail.com>
2026-09-13 23:22:52 +0900

    feat: meta tags integrations on document private file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,6 @@ import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  console.log("App COMPONENT - PAGES ROUTER");
   return <Component {...pageProps} />;
 }
